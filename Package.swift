95039891684dce67cd76e371e5b24c2ce51bb1bf
KhalilHsu <65265303+KhalilHsu@users.noreply.github.com>
2026-04-17 10:45:57 +0800

    Initial import of BrowserRouter project

---
+++ Package.swift
@@ -0,0 +1,46 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "BrowserRouter",
+    platforms: [
+        .macOS(.v14)
+    ],
+    products: [
+        .library(name: "BrowserRouterCore", targets: ["BrowserRouterCore"]),
+        .executable(name: "BrowserRouter", targets: ["BrowserRouter"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "BrowserRouterCore",
+            path: "Sources/BrowserRouterCore"
+        ),
+        .executableTarget(
+            name: "BrowserRouter",
+            dependencies: ["BrowserRouterCore"],
+            path: "Sources/BrowserRouter"
+        ),
+        .testTarget(
+            name: "BrowserRouterCoreTests",
+            dependencies: ["BrowserRouterCore"],
+            path: "Tests/BrowserRouterCoreTests",
+            swiftSettings: [
+                .unsafeFlags([
+                    "-F", "/Library/Developer/CommandLineTools/Library/Developer/Frameworks"
+                ])
+            ],
+            linkerSettings: [
+                .unsafeFlags([
+                    "-F", "/Library/Developer/CommandLineTools/Library/Developer/Frameworks",
+                    "-L", "/Library/Developer/CommandLineTools/Library/Developer/usr/lib",
+                    "-Xlinker", "-rpath",
+                    "-Xlinker", "/Library/Developer/CommandLineTools/Library/Developer/Frameworks",
+                    "-Xlinker", "-rpath",
+                    "-Xlinker", "/Library/Developer/CommandLineTools/Library/Developer/usr/lib"
+                ])
+            ]
+        )
+    ]
+)
